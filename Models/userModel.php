aba4436e64afe407270118328aad54f36b3858cc
pierre1316 <31694606+pierre1316@users.noreply.github.com>
2023-03-25 22:01:55 +0100

    bit more stuff

--- Models/userModel.php
+++ Models/userModel.php
@@ -22,7 +22,7 @@ class UserModel extends Model
         $stmt->execute([$username, $hashed_password]);
     }
 
-    public function login($acc_email, $acc_password)
+    protected function login($acc_email, $acc_password)
     {
         $query = "SELECT account.acc_email, account.acc_password, role.role AS user_role FROM account INNER JOIN role ON account.id_role = role.id WHERE acc_email = ? && acc_password = ?;";
         $stmt = $this->db->prepare($query);
@@ -52,7 +52,7 @@ class UserModel extends Model
         return false;
     }
 
-    public function logout()
+    protected function logout()
     {
         // Suppression de l'identifiant de session dans la session PHP
         unset($_SESSION['session_id']);
@@ -64,7 +64,7 @@ class UserModel extends Model
         return true;
     }
 
-    public function isauth()
+    protected function isauth()
     {
         if ($_SESSION['user_role'] == 'admin' || $_SESSION['user_role'] == 'tutor') {
             return true;
