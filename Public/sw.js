f7146855b6884c8efcca733708d4c35d3d6d4ff9
Timéo HAMLIL--BENARD <timeo.hamlilbenard@viacesi.fr>
2023-03-26 23:28:13 +0200

    a fix

---
+++ Public/sw.js
@@ -0,0 +1,56 @@
+const PREFIX = "V3";
+
+
+const CACHED_FILES = []
+
+//S'active à l'instalation de la page 
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Mettre ça pour changer de version sans devoir changer de page 
+    event.waitUntil(
+        (async () => {
+            const cache = await caches.open(PREFIX);
+            cache.add(new Request("/offline.html"));
+
+        })()
+    );
+    console.log(`${PREFIX} Install`);
+});
+
+//S'active au lancement de la page
+self.addEventListener('activate', (event) => {
+    clients.claim();//Pour que le sw prenne la main direct au chargement de la page 
+    event.waitUntil((async () => {
+        const keys = await caches.keys();
+        await Promise.all(keys.map((key) => {
+            if (!key.includes(PREFIX)) {
+                return caches.delete(key)
+            }
+        }));
+    })()
+    );
+    console.log(`${PREFIX} Active`);
+});
+
+
+self.addEventListener("fetch", (event) => {
+    console.log(`${PREFIX} Fetching : ${event.request.url}, Mode : ${event.request.mode}`);
+    if (event.request.mode == "navigate") {
+        event.respondWith(
+            (async () => {
+                try {
+                    const preloadResponse = await event.preloadResponse
+                    if (preloadResponse) {
+                        return preloadResponse
+                    }
+                    return await fetch(event.request);
+                } catch (e) {
+                    const cache = await caches.open(PREFIX);
+                    return await cache.match("/offline.html");
+
+                }
+            })()
+
+        );
+    }
+
+});
